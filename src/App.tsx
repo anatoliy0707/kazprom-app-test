8d50fd28bde1a75f8209400b0cc09576c79494cb
anatoliy0707 <anatoliy_sukhanov@mail.ru>
2022-04-13 20:39:15 +0500

    added modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
+import style from './App.module.css';
+import { List } from './component/List';
+import { AddChildModal } from './component/modal/addChildModal/AddChildModal';
+import Modal from './component/modal/Modal';
+import { ItemType } from './store/itemsReducer';
+import { AppRootStateType } from './store/store';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export enum EModeType {
+    ADD_MODE = 'ADD_MODE',
+    DELETE_MODE = 'DELETE_MODE'
 }
 
-export default App;
+function App() {
+
+    const items = useSelector<AppRootStateType, ItemType[]>(state => state.items)
+    const [mode, setMode] = useState<EModeType | null>(null)
+    const [showModal, setShowModal] = useState(false);
+
+    const onAddClickHandler = () => {
+        setShowModal(true)
+        setMode(EModeType.ADD_MODE)
+    }
+
+    return (
+        <div className={style.app}>
+            <div className={style.container}>
+                <div className={style.controlBlock}>
+                    <button onClick={onAddClickHandler}>Добавить</button>
+                    <button>Удалить</button>
+                    <button>Тест GQL</button>
+                </div>
+                <div className={style.listBlock}>
+                    <List items={items} />
+                </div>
+            </div>
+            <Modal width={395}
+                height={221}
+                enableBackground={true}
+                backgroundOnClick={() => setShowModal(false)}
+                show={showModal}
+            >
+                {mode === EModeType.ADD_MODE && <AddChildModal onSave={() => { }} />}
+            </Modal>
+        </div>
+    )
+}
+
+export default App
